e516785c774c6312562bb757702b35b6af1a4aeb
Saurav Kumar Mahato <077bct079.saurav@pcampus.edu.np>
2024-04-23 12:06:28 +0545

    changed main.jsx, App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { NavbarCustom } from './components/Navbar/Navbar';
+import AppRoute from './components/routes/AppRoute'
+
+function App() {
+
+  return (
+    <>
+      <NavbarCustom />
+      <AppRoute />
+    </>
+  )
+}
+
+export default App
